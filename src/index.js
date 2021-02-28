9c14a40410b80080e1cf89b0b8e15ace4ce51944
dizollate <66797250+dizollate@users.noreply.github.com>
2021-02-28 20:06:47 +0300

    feat: fix bag

--- src/index.js
+++ src/index.js
@@ -1,46 +1,15 @@
 module.exports = function toReadable (number) {
-  var th = ['','thousand','million', 'billion','trillion'];
-var dg = ['zero','one','two','three','four', 'five','six','seven','eight','nine'];
- var tn = ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen'];
- var tw = ['twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
-    number = number.toString();
-    number = number.replace(/[\, ]/g,'');
-    if (number != parseFloat(number)) return 'not a number';
-    var x = number.indexOf('.');
-    if (x == -1)
-        x = number.length;
-    if (x > 15)
-        return 'too big';
-    var n = number.split(''); 
-    var str = '';
-    var sk = 0;
-    for (var i=0;   i < x;  i++) {
-        if ((x-i)%3==2) { 
-            if (n[i] == '1') {
-                str += tn[Number(n[i+1])] + ' ';
-                i++;
-                sk=1;
-            } else if (n[i]!=0) {
-                str += tw[n[i]-2] + ' ';
-                sk=1;
-            }
-        } else if (n[i]!=0) { // 0235
-            str += dg[n[i]] +' ';
-            if ((x-i)%3==0) str += 'hundred ';
-            sk=1;
-        }
-        if ((x-i)%3==1) {
-            if (sk)
-                str += th[(x-i-1)/3] + ' ';
-            sk=0;
-        }
-    }
+ 	var num = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
+	var tens = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
 
-    if (x != number.length) {
-        var y = number.length;
-        str += 'point ';
-        for (var i=x+1; i<y; i++)
-            str += dg[n[i]] +' ';
-    }
-    return str.replace(/\s+/g,' ');
+    if (number < 20) return num[number];
+
+    var digit = number%10;
+    
+    if (number < 100) return tens[~~(number/10)-2] + (digit? " " + num[digit]: "");
+
+    if (number < 1000) return num[~~(number/100)] +" hundred" + (number%100 == 0? "": " " + number2words(number%100));
+
+    return number2words(~~(number/1000)) + " thousand" + (number%1000 != 0? " " + number2words(number%1000): "");
+
 }
